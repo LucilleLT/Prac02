764d082fa75f2b45b6c700fd7e3e30706eb7df61
JAVIERLT97 <javier_guay_33@hotmail.com>
2019-02-21 19:38:39 +0100

    Practica 2B Ejercicios 5-6

--- app/src/main/java/com/example/martin/prac02/QuotationActivity.java
+++ app/src/main/java/com/example/martin/prac02/QuotationActivity.java
@@ -2,19 +2,24 @@ package com.example.martin.prac02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class QuotationActivity extends AppCompatActivity {
 
+    TextView quotationText;
+    TextView authorText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quotation);
         ImageButton refreshQuotation = findViewById(R.id.refreshQuotationButton);
-        final TextView quotationText = findViewById(R.id.quotationText);
-        final TextView authorText = findViewById(R.id.authorText);
+        quotationText = findViewById(R.id.quotationText);
+        authorText = findViewById(R.id.authorText);
         quotationText.setText(String.format(getResources().getString(R.string.info_quotations), getResources().getString(R.string.username)));
 
         refreshQuotation.setOnClickListener(new View.OnClickListener() {
@@ -25,4 +30,25 @@ public class QuotationActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.quotation_activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_add:
+                return true;
+            case R.id.menu_refresh:
+                quotationText.setText(getResources().getString(R.string.sample_q));
+                authorText.setText(getResources().getString(R.string.sample_a));
+                break;
+
+        }
+        return true;
+    }
+
 }
